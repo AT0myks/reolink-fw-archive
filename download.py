f22f41362c8b4db7eb32affcd682971bd3032ea1
AT0myks <at0myks.dev@gmail.com>
2023-04-30 22:27:41 +0200

    Update download.py
    - add version flag
    - rename file with correct extension after it has been downloaded
    - print download progress
    - stronger and faster way of detecting already downloaded files
    
    Previously, the only way to determine if a file had already been
    downloaded was to compare the size given by the "content-length"
    header to the size of the target file if it exists. Not only is this
    a bad way of doing it (it's not impossible that a different file could
    have the same size), but you had to depend on and wait for the server
    to respond to get the value. This might be fine for most domains, but
    Google Drive quickly returns a 403 status after multiple requests in a
    short amount of time. There is also the fact that requests to Google
    Drive are made one by one (by default). All of this means that it
    would take a very long time for the next runs, and you'd almost always
    get errors due to the Google Drive 403s. While still being a pretty
    viable option, it pales in comparison to using the SHAs provided in
    pak_info.json. If a target file exists, its SHA is now compared to the
    'control' and if they match, no need to make a request. Combined with
    the use of asyncio.to_thread() to compute the SHAs, this significantly
    speeds up re-runs of the script (right now, around 30s vs a few minutes
    before), and requests are made only when necessary.

--- download.py
+++ download.py
@@ -2,16 +2,24 @@
 
 import argparse
 import asyncio
+import hashlib
 import logging
 from functools import partial
 from pathlib import Path
 from urllib.parse import urlparse
+from zipfile import ZipFile, is_zipfile
 
 import aiohttp
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 _LOGGER = logging.getLogger()
+
+PAK_EXT = ".pak"
+ZIP_EXT = ".zip"
+TMP_EXT = ".pakorzip"
+
+PAK_MAGIC = b"\x13Yr2"
 
 REOLINK_DOMAINS = (
     "support.reolink.com",
@@ -52,48 +60,92 @@ def find_extension(response):
     else:
         filename = ''
     if (response.content_type in ("application/zip", "application/x-zip-compressed")
-            or urlpath.endswith(".zip") or url.endswith(".zip") or filename.endswith(".zip")):
-        return ".zip"
-    elif (response.content_type in ("application/x-pak",) or urlpath.endswith(".pak")
-            or url.endswith(".pak") or filename.endswith(".pak")):
-        return ".pak"
+            or urlpath.endswith(ZIP_EXT) or url.endswith(ZIP_EXT) or filename.endswith(ZIP_EXT)):
+        return ZIP_EXT
+    elif (response.content_type in ("application/x-pak",) or urlpath.endswith(PAK_EXT)
+            or url.endswith(PAK_EXT) or filename.endswith(PAK_EXT)):
+        return PAK_EXT
     return None
 
 
-async def _download(sessions, url, path, force=False):
+def is_pakfile(file):
+    if hasattr(file, "read"):
+        magic = file.read(4)
+        file.seek(0)
+        return magic == PAK_MAGIC
+    try:
+        with open(file, "rb") as f:
+            return f.read(4) == PAK_MAGIC
+    except OSError:
+        return False
+
+
+async def _download(sessions, url, path: Path):
     session = sessions[1] if urlparse(url).netloc == "drive.google.com" else sessions[0]
     async with session.get(url) as resp:
         if not resp.ok:
             _LOGGER.log(1, f"Error {resp.status}: {url}")
             return False
         if (ext := find_extension(resp)) is not None:
-            path = path.with_name(path.name + ext)
+            path = path.with_suffix(ext)
         elif resp.content_type != "text/html":
             _LOGGER.log(1, f"Warning: could not find file type for {url}")
-            path = path.with_name(path.name + ".pakorzip")
         else:
             return False
-        if not force and path.is_file() and int(resp.headers.get("content-length", -1)) == path.stat().st_size:
-            _LOGGER.log(2, f"Already exists: {path.name}")
-            return True
         _LOGGER.log(1, f"Downloading {url} to {path}")
         with open(path, "wb") as f:
-            async for chunk in resp.content.iter_chunked(1024):
+            async for chunk in resp.content.iter_chunked(2**16):
                 f.write(chunk)
+    if path.suffix == TMP_EXT:
+        if is_zipfile(path):
+            path.rename(path.with_suffix(ZIP_EXT))
+        elif is_pakfile(path):
+            path.rename(path.with_suffix(PAK_EXT))
     return True
 
 
-async def download(sessions, devices, info, directory, force=False):
+def calc_sha256(file):
+    sha256 = hashlib.sha256()
+    if isinstance(file, (str, Path)):
+        with open(file, "rb") as f:
+            for block in iter(partial(f.read, 1024**2), b''):
+                sha256.update(block)
+    elif hasattr(file, "read"):
+        for block in iter(partial(file.read, 1024**2), b''):
+            sha256.update(block)
+    else:
+        raise TypeError("file must be a file or file-like object")
+    return sha256.hexdigest()
+
+
+def already_downloaded(path: Path, sha256: str):
+    as_pak = path.with_suffix(PAK_EXT)
+    as_zip = path.with_suffix(ZIP_EXT)
+    if as_pak.is_file():
+        return calc_sha256(as_pak) == sha256
+    elif is_zipfile(as_zip):
+        with ZipFile(as_zip) as myzip:
+            for name in myzip.namelist():
+                with myzip.open(name) as file:
+                    if is_pakfile(file) and calc_sha256(file) == sha256:
+                        return True
+    return False
+
+
+async def download(sessions, devices, sha256: str, info, directory: Path, force=False):
     model, hw_ver = get_names(devices, info["model_id"], info["hw_ver_id"])
     version = info["info"]["firmware_version_prefix"] + '.' + info["info"]["version_file"]
     filename = "__".join((model, hw_ver, version))
-    path = directory / model / hw_ver.strip() / filename
+    path = directory / model / hw_ver.strip() / (filename + TMP_EXT)
+    if not force and (await asyncio.to_thread(already_downloaded, path, sha256)):
+        _LOGGER.log(2, f"Already exists (sha256): {path.stem}")
+        return True, None, None
     path.parent.mkdir(parents=True, exist_ok=True)
     exception = None
     # Prioritize original Reolink URLs.
     for url in sorted(info["download"], key=is_original_url, reverse=True):
         try:
-            if await _download(sessions, url, path, force):
+            if await _download(sessions, url, path):
                 return True, url, None
         except Exception as e:
             exception = e
@@ -107,8 +159,9 @@ async def main():
     parser.add_argument("-d", "--directory", type=Path, default=Path.cwd() / "reolink-archive", help="directory where the files will be downloaded. Default: in a new dir created in the current one")
     parser.add_argument("-g", "--max-connections-gdrive", type=int, default=1, help="max simultaneous downloads for Google Drive links. Default: %(default)s")
     parser.add_argument("-m", "--max-connections", type=int, default=20, help="max simultaneous downloads for other links. Default: %(default)s")
-    parser.add_argument("-f", "--force", action="store_true", help="download even if file exists")
-    parser.add_argument("--verbose", "-v", action="count", default=0, help="increase verbosity. Can appear multiple times")
+    parser.add_argument("-f", "--force", action="store_true", help="download even if file exists (overwrite)")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="increase verbosity. Can appear multiple times")
+    parser.add_argument("-V", "--version", action="version", version=f"%(prog)s {__version__}")
     args = parser.parse_args()
 
     def filter(verbosity, record):
@@ -121,16 +174,31 @@ async def main():
     sh.setFormatter(logging.Formatter("%(message)s"))
     _LOGGER.addHandler(sh)
 
+    count = 0
+
+    async def print_progress(target):
+        while count != target:
+            print(f"{count}/{target}", end='\r')
+            await asyncio.sleep(.1)
+
+    def callback(fut):
+        nonlocal count
+        count += 1
+
     conn = aiohttp.TCPConnector(limit=args.max_connections)
     conn_drive = aiohttp.TCPConnector(limit=args.max_connections_gdrive)
     async with (aiohttp.ClientSession(connector=conn) as session,
-            aiohttp.ClientSession(connector=conn_drive) as session_drive):
+                aiohttp.ClientSession(connector=conn_drive) as session_drive):
         pak_info = await fetch(session, "https://raw.githubusercontent.com/AT0myks/reolink-fw-archive/main/pak_info.json")
         devices = await fetch(session, "https://raw.githubusercontent.com/AT0myks/reolink-fw-archive/main/devices.json")
         sessions = [session, session_drive]
-        coros = [download(sessions, devices, info, args.directory, args.force) for info in pak_info.values()]
-        print(f"Downloading {len(coros)} firmwares")
-        results = await asyncio.gather(*coros)
+        tasks = [asyncio.create_task(download(sessions, devices, sha256, info, args.directory, args.force)) for sha256, info in pak_info.items()]
+        for task in tasks:
+            task.add_done_callback(callback)
+        print(f"Downloading {len(tasks)} firmwares")
+        if args.verbose == 0:
+            asyncio.create_task(print_progress(len(tasks)))
+        results = await asyncio.gather(*tasks)
     successes = failures = 0
     for success, url, exception in results:
         if exception is not None:
